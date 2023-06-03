210e5dca1f6a1eec61ececbc29db0538faf133bf
Roamin <roamin@tuta.io>
2023-06-03 06:05:27 +0000

    fix buffer

--- deno/deps.ts
+++ deno/deps.ts
@@ -2,7 +2,7 @@ export { INamespace } from "https://esm.sh/protobufjs@7.2.3"
 export * as protobufjs from "https://esm.sh/protobufjs@7.2.3"
 export * as fetch from "https://esm.sh/cross-fetch@3.1.5";
 export * as koinosPbToProto from "https://esm.sh/@roamin/koinos-pb-to-proto@0.0.3";
-export { Buffer } from "https://deno.land/std@0.114.0/node/buffer.ts";
+export { Buffer } from "http://deno.land/x/node_buffer@1.1.0/mod.ts";
 export { sha256 } from "https://esm.sh/@noble/hashes@1.1.2/sha256";
 export * as secp from "https://esm.sh/@noble/secp256k1@1.6.3";
 export * as multibase from "https://esm.sh/multibase@4.0.6";
